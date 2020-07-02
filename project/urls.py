3ab8bfcc012699f6604295700856e74cf937e14f
Sumod Sivadas <contactsumod@gmail.com>
2020-07-02 23:13:51 +0530

    final commit

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from todo.views import todo, addTodo, deleteTodo
-
 urlpatterns = [
     path('todo/', todo),
     path('admin/', admin.site.urls),
